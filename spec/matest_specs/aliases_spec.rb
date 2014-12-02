a946942fb975b884a72752df4a8e4ce0a0834432
Federico Iachetti <iachetti.federico@gmail.com>
2014-12-02 02:05:15 -0300

    Added tests for aliases

---
+++ spec/matest_specs/aliases_spec.rb
@@ -0,0 +1,65 @@
+scope do
+  spec { true }
+  xspec { true }
+
+  example { true }
+  xexample { true }
+
+  it { true }
+  xit { true }
+end
+
+describe do
+  spec { true }
+  xspec { true }
+
+  example { true }
+  xexample { true }
+
+  it { true }
+  xit { true }
+end
+
+context do
+  spec { true }
+  xspec { true }
+
+  example { true }
+  xexample { true }
+
+  it { true }
+  xit { true }
+end
+
+xscope do
+  spec { true }
+  xspec { true }
+
+  example { true }
+  xexample { true }
+
+  it { true }
+  xit { true }
+end
+
+xdescribe do
+  spec { true }
+  xspec { true }
+
+  example { true }
+  xexample { true }
+
+  it { true }
+  xit { true }
+end
+
+xcontext do
+  spec { true }
+  xspec { true }
+
+  example { true }
+  xexample { true }
+
+  it { true }
+  xit { true }
+end
