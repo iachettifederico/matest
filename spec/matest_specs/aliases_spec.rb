13509ebb49d488f790f23dcf5d1855a125952717
Jeffrey Horn <jeffh@generalassemb.ly>
2014-12-03 16:55:48 -0500

    Make skipped specs fail if aliases are broken
    Previously, skip aliases would error if they were not defined. This
    change ensures that skipped specs will fail if, for some reason,
    skip aliases are overwritten but not undefined.

--- spec/matest_specs/aliases_spec.rb
+++ spec/matest_specs/aliases_spec.rb
@@ -1,65 +1,65 @@
 scope do
   spec { true }
-  xspec { true }
+  xspec { false }
 
   example { true }
-  xexample { true }
+  xexample { false }
 
   it { true }
-  xit { true }
+  xit { false }
 end
 
 describe do
   spec { true }
-  xspec { true }
+  xspec { false }
 
   example { true }
-  xexample { true }
+  xexample { false }
 
   it { true }
-  xit { true }
+  xit { false }
 end
 
 context do
   spec { true }
-  xspec { true }
+  xspec { false }
 
   example { true }
-  xexample { true }
+  xexample { false }
 
   it { true }
-  xit { true }
+  xit { false }
 end
 
 xscope do
   spec { true }
-  xspec { true }
+  xspec { false }
 
   example { true }
-  xexample { true }
+  xexample { false }
 
   it { true }
-  xit { true }
+  xit { false }
 end
 
 xdescribe do
   spec { true }
-  xspec { true }
+  xspec { false }
 
   example { true }
-  xexample { true }
+  xexample { false }
 
   it { true }
-  xit { true }
+  xit { false }
 end
 
 xcontext do
   spec { true }
-  xspec { true }
+  xspec { false }
 
   example { true }
-  xexample { true }
+  xexample { false }
 
   it { true }
-  xit { true }
+  xit { false }
 end
