ec73e3a93f5b1e00d4f4144023b40010d09ee7be
Federico Iachetti <iachetti.federico@gmail.com>
2015-01-14 21:34:26 -0300

    Added slow specs (skipped) to test printing

---
+++ spec/matest_specs/slow_spec.rb
@@ -0,0 +1,9 @@
+xscope "slow" do
+  spec { sleep(0.5); true }
+  spec { sleep(0.5); true }
+  spec { sleep(0.5); true }
+  spec { sleep(0.5); true }
+  spec { sleep(0.5); true }
+  spec { sleep(0.5); true }
+  spec { sleep(0.5); true }
+end
