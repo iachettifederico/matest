fcd7fc1091c7f6191a15720bcff51a34ea509dc2
Federico Iachetti <iachetti.federico@gmail.com>
2015-01-19 20:36:35 -0300

    Fixed a bug in capture_exception

--- lib/matest/capture_exception.rb
+++ lib/matest/capture_exception.rb
@@ -1,14 +1,23 @@
-class NoExceptionRaised < RuntimeError; end
-class UnexpectedExceptionRaised < RuntimeError; end
+module Matest
+  class NoExceptionRaised < RuntimeError; end
+  class UnexpectedExceptionRaised < RuntimeError; end
 
-def capture_exception(exception_class=BasicObject, &block)
-  begin
-    block.call
-    raise NoExceptionRaised.new
-    false
-  rescue exception_class => e
-    e
-  rescue BasicObject => e
-    raise UnexpectedExceptionRaised.new
+  def capture_exception(exception_class=nil, &block)
+    expected_exception = exception_class || BasicObject
+    begin
+      block.call
+      if exception_class
+        raise Matest::NoExceptionRaised.new("Expected '#{exception_class.inspect}' from the block, but none was raised.")
+      else
+        raise Matest::NoExceptionRaised.new("Expected an Exception from the block, but none was raised.")
+      end
+      false
+    rescue Matest::NoExceptionRaised => e
+      raise e
+    rescue expected_exception => e
+      e
+    rescue BasicObject => e
+      raise e
+    end
   end
 end
